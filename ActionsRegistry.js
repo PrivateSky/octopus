d72dfa6990b6804d2d07721eec2f022a4a31d7ed
Daniel Visoiu <9691701+daniel-visoiu@users.noreply.github.com>
2022-11-28 10:02:34 +0200

    Added branch options to smartClone action

--- ActionsRegistry.js
+++ ActionsRegistry.js
@@ -352,6 +352,9 @@ function ActionsRegistry() {
             } else {
                 global.collectLog = true;
             }
+            if(typeof action.branch === "string") {
+                options.branch = action.branch;
+            }
 
             if (typeof action.commit !== "undefined") {
                 //Do a shallow clone (for a specific commit)
